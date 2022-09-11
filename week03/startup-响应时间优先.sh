93b67838b984a5022ffdb4a1dca7128d73d90ec5
Chixiami <102499854+Chixiami@users.noreply.github.com>
2022-09-12 02:11:18 +0800

    JVM压测

---
+++ week03/startup-响应时间优先.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Copyright 1999-2022 Geek NB Group Holding Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#export JAVA_HOME=/usr/local/hero/jdk1.8.0_261
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
+
+#===========================================================================================
+# init
+#===========================================================================================
+
+export SERVER="ruoyi-admin"
+export JAVA_HOME
+export JAVA="$JAVA_HOME/bin/java"
+# 获取当前目录
+export BASE_DIR=`cd $(dirname $0)/.; pwd`
+
+#===========================================================================================
+# JVM Configuration
+#===========================================================================================
+JAVA_OPT="${JAVA_OPT} -Xms256m -Xmx256m -Xmn125m -XX:MetaspaceSize=128m -Xss512k"
+JAVA_OPT="${JAVA_OPT} -XX:+UseParNewGC -XX:+UseConcMarkSweepGC "
+JAVA_OPT="${JAVA_OPT} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -Xloggc:${BASE_DIR}/logs/gc-parnew-cms.log"
+JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
+JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
+JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/${SERVER}.jar"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+# 创建日志文件目录
+if [ ! -d "${BASE_DIR}/logs" ]; then
+  mkdir ${BASE_DIR}/logs
+fi
+
+# 输出变量
+echo "$JAVA ${JAVA_OPT}"
+# 检查start.out日志输出文件
+if [ ! -f "${BASE_DIR}/logs/${SERVER}.out" ]; then
+  touch "${BASE_DIR}/logs/${SERVER}.out"
+fi
+#===========================================================================================
+# 启动服务
+#===========================================================================================
+# 启动服务
+echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/${SERVER}.out 2>&1 &
+nohup $JAVA ${JAVA_OPT} ruoyi-admin.ruoyi-admin >> ${BASE_DIR}/logs/${SERVER}.out 2>&1 &
+echo "server is starting，you can check the ${BASE_DIR}/logs/${SERVER}.out"
