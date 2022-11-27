b93b3488b03ade5e31138bb7a650290101989e52
chenhan <chehan@sinosoft.com.cn>
2022-11-27 22:04:15 +0800

    第十四周作业 陈瀚

--- week12/MyBatis问答题.md
+++ week12/MyBatis问答题.md
@@ -1,5 +1,6 @@
 # MyBatis问答题
 ## 题目01
+MyBatis Dao 接口的工作原理是使用了jdk的动态代理
 
 ## 题目02
 
---
+++ week14/myherobatis-frame/src/main/java/com/myhero/frame/openapi/SqlSession.java
@@ -0,0 +1,17 @@
+package com.myhero.frame.openapi;
+
+import java.util.List;
+
+/**
+ * 对外暴露SqlSession接口
+ * 接口中定义常见的JDBC操作：增删改查
+ */
+public interface SqlSession {
+
+    /**
+     * 查询所有用户
+     * T 代表泛型类型，T(type缩写)
+     */
+    <T> List<T> selectList(String sql) throws Exception;
+
+}
